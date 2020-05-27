220376d009af20a408b50dfe8bc3468509511cd3
12321312 <xshadownightx12@gmail.com>
2020-05-28 02:06:56 +0700

    Update nabor.js

--- cmds/user/nabor.js
+++ cmds/user/nabor.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     let infore = new Discord.RichEmbed()
-    .addField("Набор на пост администрации серверов:", '`DarkRP` - https://svyanov.com/redirect?s=2\n`SCP-RP` - https://svyanov.com/redirect?s=7\n`MilitaryRP` - https://vk.cc/auIPcX\n\n`Набор на пост ивентера` - https://vk.cc/auIPro' , true);
+    .addField("**Набор на пост администрации серверов:**", '`DarkRP` - https://svyanov.com/redirect?s=2\n`SCP-RP` - https://svyanov.com/redirect?s=7\n`MilitaryRP` - https://vk.cc/auIPcX\n`Набор на пост ивентера` - https://vk.cc/auIPro' , true);
     message.reply({embed:infore});
 };
 module.exports.command = {
