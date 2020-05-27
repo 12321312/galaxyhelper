4ec07a280cf7c6af961d91c17f8356ccc612f257
12321312 <xshadownightx12@gmail.com>
2020-05-28 01:21:44 +0700

    Update role.js

--- cmds/user/role.js
+++ cmds/user/role.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
-    message.reply('Правила серверов и discord:\nDarkRP` - https://svyanov.com/redirect?s=0\n`Правила SCP-RP` - https://svyanov.com/redirect?s=6\n`Правила MilitaryRP` - https://svyanov.com/redirect?s=8\n`Правила Discord` - https://docs.google.com/document/d/1DeTwuIwS6GY8A9mp7J1gD304meQ8DlS2-yxZmDUEleo');
+    message.reply('Правила серверов и discord:\n`DarkRP` - https://svyanov.com/redirect?s=0\n`Правила SCP-RP` - https://svyanov.com/redirect?s=6\n`Правила MilitaryRP` - https://svyanov.com/redirect?s=8\n`Правила Discord` - https://docs.google.com/document/d/1DeTwuIwS6GY8A9mp7J1gD304meQ8DlS2-yxZmDUEleo');
 };
 module.exports.command = {
     name: 'role',
