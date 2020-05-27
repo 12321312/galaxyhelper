78fc55071798b6c23d7bbc5ff51af9c5a3bc8f4d
12321312 <xshadownightx12@gmail.com>
2020-05-28 01:19:41 +0700

    1

---
+++ cmds/user/nabor.js
@@ -0,0 +1,13 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+exports.run = async (bot, message, args) => { 
+    message.reply('**Набор на пост администрации серверов:**/nDarkRP - https://svyanov.com/redirect?s=2/n`SCP-RP` - https://svyanov.com/redirect?s=7/n`MilitaryRP` - https://forms.gle/cdFXNYKZsht3Qrnx6/n/n`Набор на пост ивентера` - https://forms.gle/aMRvQW99LA8dUpFK9');
+};
+module.exports.command = {
+    name: 'nabor',
+    aliases: ["набор", "наборвадмины", "наборбл"],
+    description: "Показывает пинг, чо доебался?",
+    usage: "usercommand", 
+    category: "user",
+    enabled: true
+};  
