53828221cfaabebef8a67c80817d819e39e3dffd
12321312 <xshadownightx12@gmail.com>
2020-05-28 01:24:01 +0700

    1

--- cmds/user/nabor.js
+++ cmds/user/nabor.js
@@ -1,7 +1,7 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
-    message.reply('**Набор на пост администрации серверов:**/nDarkRP - https://svyanov.com/redirect?s=2/n`SCP-RP` - https://svyanov.com/redirect?s=7/n`MilitaryRP` - https://forms.gle/cdFXNYKZsht3Qrnx6/n/n`Набор на пост ивентера` - https://forms.gle/aMRvQW99LA8dUpFK9');
+    message.reply('**Набор на пост администрации серверов:**\nDarkRP - https://svyanov.com/redirect?s=2/n`SCP-RP` - https://svyanov.com/redirect?s=7/n`MilitaryRP` - https://forms.gle/cdFXNYKZsht3Qrnx6/n/n`Набор на пост ивентера` - https://forms.gle/aMRvQW99LA8dUpFK9');
 };
 module.exports.command = {
     name: 'nabor',
