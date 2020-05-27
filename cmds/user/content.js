2b0a7d1733a0a011c0ed3d1e8add43f4a3fe931a
12321312 <xshadownightx12@gmail.com>
2020-05-28 02:11:44 +0700

    1

--- cmds/user/content.js
+++ cmds/user/content.js
@@ -1,7 +1,9 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
-    message.reply('**ссылки на скачивание контента наших серверов:**\n`Контент DarkRP` - https://vk.cc/8v2keP\n`Контент SCP-RP` - https://vk.cc/8SO3ho\n`Контент MilitaryRP` - https://vk.cc/auIPwZ');
+    let infore = new Discord.RichEmbed()
+    .addField("**ссылки на скачивание контента наших серверов:**", '`Контент DarkRP` - https://vk.cc/8v2keP\n`Контент SCP-RP` - https://vk.cc/8SO3ho\n`Контент MilitaryRP` - https://vk.cc/auIPwZ' , true);
+    message.reply({embed:infore});
 };
 module.exports.command = {
     name: 'content',
--- cmds/user/role.js
+++ cmds/user/role.js
@@ -1,7 +1,10 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
-    message.reply('**актуальные ссылки на правила серверов и discord:**\n`DarkRP` - https://svyanov.com/redirect?s=0\n`Правила SCP-RP` - https://svyanov.com/redirect?s=6\n`Правила MilitaryRP` - https://svyanov.com/redirect?s=8\n`Правила Discord` - https://vk.cc/auIPim');
+    let infore = new Discord.RichEmbed()
+    .addField("**Актуальные ссылки на правила проекта:**", '`DarkRP` - https://svyanov.com/redirect?s=0\n`Правила SCP-RP` - https://svyanov.com/redirect?s=6\n`Правила MilitaryRP` - https://svyanov.com/redirect?s=8\n`Правила Discord` - https://vk.cc/auIPim' , true);
+    message.reply({embed:infore});
+
 };
 module.exports.command = {
     name: 'role',
