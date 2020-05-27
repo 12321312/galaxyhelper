14d1e287cafb04a8658b815d4e9bd644bc4f2c0a
12321312 <xshadownightx12@gmail.com>
2020-05-28 02:15:32 +0700

    Update content.js

--- cmds/user/content.js
+++ cmds/user/content.js
@@ -2,7 +2,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     let infore = new Discord.RichEmbed()
-    .addField("**ссылки на скачивание контента наших серверов:**", '`Контент DarkRP` - https://vk.cc/8v2keP\n`Контент SCP-RP` - https://vk.cc/8SO3ho\n`Контент MilitaryRP` - https://vk.cc/auIPwZ' , true);
+    .addField("**Ссылки на скачивание контента наших серверов:**", '`Контент DarkRP` - https://vk.cc/8v2keP\n`Контент SCP-RP` - https://vk.cc/8SO3ho\n`Контент MilitaryRP` - https://vk.cc/auIPwZ' , true);
     message.reply({embed:infore});
 };
 module.exports.command = {
