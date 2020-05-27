0256b408c19a2b00b3e1abd059948ac9e37fd2ad
12321312 <xshadownightx12@gmail.com>
2020-05-28 01:09:22 +0700

    Create admin.js

---
+++ cmds/admin/admin.js
@@ -0,0 +1,13 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+exports.run = async (bot, message, args) => { 
+    message.reply('Правила DarkRP - https://svyanov.com/redirect?s=0\nПравила SCP-RP - https://svyanov.com/redirect?s=6\nMilitaryRP - https://docs.google.com/document/d/1SfuAcAYXILoBPwAewMYFpJUqA6KWNxbMnISZb3B0JaE/edit#heading=h.28yu4yk7jaag\nDiscord - https://docs.google.com/document/d/1DeTwuIwS6GY8A9mp7J1gD304meQ8DlS2-yxZmDUEleo/edit?usp=drivesdk');
+};
+module.exports.command = {
+    name: '123123',
+    aliases: ["пра3123123вила", "пр123123авило", "правил123123абл"],
+    description: "Показывает пинг, чо доебался?",
+    usage: "usercommand", 
+    category: "user",
+    enabled: true
+};  
