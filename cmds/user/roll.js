88d57e0a3f3806ab252b98515ec525db2d4dbaaf
12321312 <xshadownightx12@gmail.com>
2020-06-22 00:54:50 +0700

    Update roll.js

--- cmds/user/roll.js
+++ cmds/user/roll.js
@@ -3,7 +3,7 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     let botmessage = args.join(" ");
-    if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю'); 
+    if(!message.member.roles.some(r=>["Помощь проекта", "Команда сервера", "Менеджер проекта", "Discord-admin"].includes(r.name))) return message.reply('Отказано в доступе.');
     if (!(args[0])) return message.reply('Пустота в обращении...');
     message.delete()
     let mutechannel = message.guild.channels.get("724224485812600853");
