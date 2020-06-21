810f8454830e0bfd2fd521faed09a40c88c5bcb9
12321312 <xshadownightx12@gmail.com>
2020-06-22 00:46:07 +0700

    Create roll.js

---
+++ cmds/user/roll.js
@@ -0,0 +1,20 @@
+
+const Discord = module.require("discord.js");
+const fs = require("fs");
+exports.run = async (bot, message, args) => { 
+    let botmessage = args.join(" ");
+    if (message.author.id !== "294844223675564034") return message.reply('Хитрожопых наказываю'); 
+    if (!(args[0])) return message.reply('Пустота в обращении...');
+    message.delete()
+    let mutechannel = message.guild.channels.get("724224485812600853");
+    mutechannel.send(botmessage);
+
+};
+module.exports.command = {
+    name: 'canal',
+    aliases: ["канал", "отправь", "отправить"],
+    description: "Отправляет в канал какую-то дичь, чо доебался?",
+    usage: "foxcommand",
+    category: "fox",
+    enabled: false
+}; 
